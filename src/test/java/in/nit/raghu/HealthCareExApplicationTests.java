cf199bf3119932c9266219989f89b1e08c749045
tirunprasad <tirunprasad27@gmail.com>
2021-09-21 12:41:01 +0530

    FirstCommit

---
+++ src/test/java/in/nit/raghu/HealthCareExApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nit.raghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
